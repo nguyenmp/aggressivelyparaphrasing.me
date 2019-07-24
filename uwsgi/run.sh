81c3da1cf4881badf590fcb8f1a70dc29f34bcc9
Mark Nguyen <mnguyen4@apple.com>
2019-07-24 04:28:18 -0700

    Created a run script for uwsgi

---
+++ uwsgi/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# NearlyFreeSpeech prevents arguments in their UI so they recommend a "run" script
+# https://members.nearlyfreespeech.net/faq?q=RunScript#RunScript
+
+exec uwsgi --socket 127.0.0.1:3031 --wsgi scorsese --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191
