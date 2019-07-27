8ffd2c7c177e2ca71997aa52c07b97edf4bde4ba
Me 268909 <268909@aggressivelyparaphrasingmyself.nfshost.com>
2019-07-27 11:19:27 +0000

    Added SECRET_KEY generation when to run.sh for production

--- uwsgi/run.sh
+++ uwsgi/run.sh
@@ -3,4 +3,5 @@
 # NearlyFreeSpeech prevents arguments in their UI so they recommend a "run" script
 # https://members.nearlyfreespeech.net/faq?q=RunScript#RunScript
 
+export SECRET_KEY=$(head -c 64 /dev/urandom | openssl enc -base64)
 exec uwsgi --socket 127.0.0.1:3031 --wsgi scorsese --callable app --processes 4 --threads 2 --stats 127.0.0.1:9191 --logto /home/private/uwsgi.log
